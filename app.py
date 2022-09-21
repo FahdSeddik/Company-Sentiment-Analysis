83c4957e4681620e94c3b0ee9ae4da6d9d68f969
Fahd Seddik <62207434+FahdSeddik@users.noreply.github.com>
2022-09-21 02:36:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -138,44 +138,20 @@ def main():
             )
 
             #Word Cloud
-            weirdPatterns = re.compile("["
-                               u"\U0001F600-\U0001F64F"  # emoticons
-                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                               u"\U00002702-\U000027B0"
-                               u"\U000024C2-\U0001F251"
-                               u"\U0001f926-\U0001f937"
-                               u'\U00010000-\U0010ffff'
-                               u"\u200d"
-                               u"\u2640-\u2642"
-                               u"\u2600-\u2B55"
-                               u"\u23cf"
-                               u"\u23e9"
-                               u"\u231a"
-                               u"\u3030"
-                               u"\ufe0f"
-                               u"\u2069"
-                               u"\u2066"
-                               u"\u200c"
-                               u"\u2068"
-                               u"\u2067"
-                               "]+", flags=re.UNICODE)
-            words = " ".join(word for tweet in tweets for word in tweet.split())
-            words = weirdPatterns.sub(r'', words)
-            st_en = set(stopwords.words('english'))
-            st_ar = set(stopwords.words('arabic'))
-            if opt=='Arabic':
-                words = arabic_reshaper.reshape(words)
-                words = get_display(words)
-            st_en = st_en.union(STOPWORDS).union(set(['https','http','DM','dm','via','co']))
-            wordcloud = WordCloud(stopwords=st_en.union(st_ar), background_color="white", width=800, height=400,font_path='NotoNaskhArabic-Regular.ttf')
-            wordcloud.generate(words)
-            fig2, ax2 = plt.subplots(figsize=(5,5))
-            ax2.axis("off")
-            fig2.tight_layout(pad=0)
-            ax2.imshow(wordcloud, interpolation='bilinear')
-            st.pyplot(fig2)
+            if opt=='English':
+                words = " ".join(word for tweet in tweets for word in tweet.split())
+                st_en = set(stopwords.words('english'))
+                #st_ar = set(stopwords.words('arabic'))
+                st_en = st_en.union(STOPWORDS).union(set(['https','http','DM','dm','via','co']))
+                wordcloud = WordCloud(stopwords=st_en, background_color="white", width=800, height=400)
+                wordcloud.generate(words)
+                fig2, ax2 = plt.subplots(figsize=(5,5))
+                ax2.axis("off")
+                fig2.tight_layout(pad=0)
+                ax2.imshow(wordcloud, interpolation='bilinear')
+                st.pyplot(fig2)
+            else:
+                st.error('WordCloud not available for Language = {opt}')
 
     else:
         st.subheader("About")
